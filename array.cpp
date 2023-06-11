f0f1ff6684bc8af821161ccdc6492d3e26c410f3
Ishita Thardak <120077126+ishita1618@users.noreply.github.com>
2023-06-11 13:36:52 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int num[ 5 ] = {1, 2, 3, 4 ,5 };      //creating array . Array are used to store multiple values in single variable
+  cout << "Done";                       // printing output 
+
+  return 0;
+}
